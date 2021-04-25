03f77c1ca03eb06008a531b10dcb6afd76cda97f
daniElPapu <daniel.bedergal@tecsup.edu.pe>
2021-04-24 22:47:32 -0500

    Segundo commit se agregó la información de la cantidad de registros y la hora de la solicitud

--- server.js
+++ server.js
@@ -2,34 +2,37 @@ var express = require('express') //llamamos a Express
 var app = express()               
 
 var port = process.env.PORT || 3001  // establecemos nuestro puerto
-
+var persons =[
+    { 
+        id: 1, 
+        name: "Arto Hellas",
+        number: "040-123456"
+    },
+    {
+        id: 2,
+        name: "Ada Lovelace",
+        number: "39-44-5323523"
+    },
+    {
+        id: 3,
+        name: "Dan Abramov",
+        number: "12-43-234345"
+    },
+    {
+        id: 4,
+        name: "Mary Poppendick",
+        number: "39-23-6423122"
+    },
+]
 app.get('/', function(req, res) {
-  res.json({ mensaje: '¡Hola Mundo!' })   
+    res.json({ mensaje: '¡Hola Mundo!' })   
 })
 app.get('/api/persons', function(req, res) {
-    res.json([
-        { 
-            id: 1, 
-            name: "Arto Hellas",
-            number: "040-123456"
-        },
-        {
-            id: 2,
-            name: "Ada Lovelace",
-            number: "39-44-5323523"
-        },
-        {
-            id: 3,
-            name: "Dan Abramov",
-            number: "12-43-234345"
-        },
-        {
-            id: 4,
-            name: "Mary Poppendick",
-            number: "39-23-6423122"
-        },
-    ])   
+    res.json(persons)   
   })
+app.get('/info/', function(req, res) {
+    res.send("Phonebook has info for "+persons.length +" people <br> "+new Date()+"")   
+})
 // iniciamos nuestro servidor
 app.listen(port)
 console.log('API escuchando en el puerto ' + port)
